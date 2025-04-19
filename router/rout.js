05426d9ef33842e5829f869466b15991417da697
TufanRai <thulungtufan16@gmail.com>
2025-04-19 08:24:27 +0545

    applied some message to the code to understand which code is performing what operation

--- router/rout.js
+++ router/rout.js
@@ -1,16 +1,20 @@
 const express = require('express')
-const router = express.Router()
+const router = express.Router() // invoking the router form the express
 
-router.get('/', (req,res) => {
+router.get('/', (req,res) => { // handles the requests of the users in the home page
     res.send('this is a home page')
 })
 
-router.get('/users', (req,res) => {
+router.get('/users', (req,res) => { // handles the requiest of the users in the users page
     res.send('this is a users page')
 })
 
-router.get('/users/:id', (req,res) => {
+router.get('/users/:id', (req,res) => { // helps users to find the data with its' id
     res.send(`this is a page of a user having id ${req.params.id}`)
 })
 
-module.exports = router
+router.get('/*e', (req,res) => { // handles the file not found section
+    res.status(404).send('the page you are looking for is not found')
+})
+
+module.exports = router // exporting the router
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const route = require('../routing-routs/router/rout.js')
+const route = require('../routing-routs/router/rout.js') // accessing the router 
 const app = express()
 
-app.use('/', route)
+app.use('/', route) // appling the router level request responce handling
 
 app.listen(8000, () => {
     console.log('server started at http://localhost:8000')
